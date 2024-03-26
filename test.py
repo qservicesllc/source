9e7bae7d59efa8cabf150d6364cdff48265a2b4d
Bret Quackenbush <bquackenbush@qservicesllc.com>
2024-03-26 05:22:32 +0000

    ok

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
 print("This is a test.")
+# Define variables
+# userAge = 53
+# userAge, userName = 10.5, 'Bret'
+# print('Your name is %d, and your age is %s'%(userAge, userName))
+# print('Your name is {0}, and your age is {1}'.format(userAge, userName))
+# x, y =5, 2
+# # Addition
+# print(x+y)
+# # Division
+# print(x/y)
+# # Multiplication
+# print(x*y)
